7b782d6a4d652c664760d90e4147cedd7d23efc3
wagner@dextak.com.br <ttLLW6v9ETSJLXnEmMyD>
2024-03-27 17:03:22 -0300

    teste exception

--- android/library/src/main/java/com/polidea/multiplatformbleadapter/errors/ErrorConverter.java
+++ android/library/src/main/java/com/polidea/multiplatformbleadapter/errors/ErrorConverter.java
@@ -79,6 +79,7 @@ public class ErrorConverter {
         }
 
         if (throwable instanceof BleDisconnectedException) {
+            System.out.println('edgar e brenda');
             BleDisconnectedException bleDisconnectedException = (BleDisconnectedException) throwable;
             BleError bleError = new BleError(BleErrorCode.DeviceDisconnected, throwable.getMessage(), bleDisconnectedException.state);
             bleError.deviceID = bleDisconnectedException.bluetoothDeviceAddress;
